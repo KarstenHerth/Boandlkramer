87f119265b445ed3378a54b4349cad787d0e16e1
MFaigle <33434117+MFaigle@users.noreply.github.com>
2017-11-22 19:18:38 +0100

    walking

---
+++ Boandlkramer/Assets/Animation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Animation : MonoBehaviour {
+
+    public Animator anim;
+    NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+        nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = nav.velocity.magnitude/nav.speed;
+        anim.SetFloat("Speed", move);
+	}
+}
