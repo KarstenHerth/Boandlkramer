e483a88445f8701f2d2cf2cabeada35374ff0b24
Felix <felix.kammerlander@gmail.com>
2018-06-04 19:25:56 +0200

    slightly nicer health & mana bar + Inventory, Characterscreen & Skilltree are Drag&Drop windows

---
+++ Boandlkramer/Assets/Scripts/UI/DragDropWindow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class DragDropWindow : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    [SerializeField]
+    Transform windowParent;
+
+    Vector2 offset;
+
+    void Start()
+    {
+        if (windowParent)
+        {
+            offset = this.transform.position - windowParent.position;
+        }
+        else
+        {
+            Debug.LogError("DragDropWindow needs assigned windowParent (Transform)");
+        }
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (windowParent)
+        {
+            windowParent.position = eventData.position - offset;
+        }
+    }
+
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+
+    }
+}
